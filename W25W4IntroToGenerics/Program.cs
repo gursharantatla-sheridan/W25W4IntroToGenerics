3d7b8802de7080c361e2911c94dc83b3cf04e0dd
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-31 16:36:25 -0500

    intro to generics

--- W25W4IntroToGenerics/Program.cs
+++ W25W4IntroToGenerics/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num = 5;
+
+            object obj = num;   // boxing
+
+            num = (int)obj;     // unboxing
+
+
+
+            //if (AreEqual(5, "hello"))
+            if (AreEqual<int>(5, 5))
+                Console.WriteLine("Both are equal");
+            else
+                Console.WriteLine("Both are not equal");
+        }
+
+        // non-generic method
+        static bool AreEqual(object value1, object value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        // generic method
+        static bool AreEqual<T>(T value1, T value2)
+        {
+            return value1.Equals(value2);
         }
     }
 }
