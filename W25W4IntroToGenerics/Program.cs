d52998930198954b00de4c48b67a45a35b52537d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-31 16:43:18 -0500

    printarray

--- W25W4IntroToGenerics/Program.cs
+++ W25W4IntroToGenerics/Program.cs
@@ -17,7 +17,29 @@
                 Console.WriteLine("Both are equal");
             else
                 Console.WriteLine("Both are not equal");
+
+
+
+            int[] intArray = { 4, 5, 6, 7, 8 };
+            double[] doubleArray = { 3.3, 4.4, 5.5, 6.6 };
+            char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+
+            PrintArray<int>(intArray);
+            PrintArray<double>(doubleArray);
+            PrintArray<char>(charArray);
         }
+
+
+        static void PrintArray<T>(T[] array)
+        {
+            Console.WriteLine("\n\nNumber of items: " + array.Length);
+
+            foreach (var i in array)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+
+
 
         // non-generic method
         static bool AreEqual(object value1, object value2)
